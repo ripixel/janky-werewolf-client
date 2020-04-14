fb71b935829111a7ca096d5ecdc7dc74d26552ea
James King <ripixel@gmail.com>
2020-04-14 17:40:58 +0100

    Disabled test coverage reqs

--- jest.config.js
+++ jest.config.js
@@ -11,16 +11,16 @@ module.exports = {
     '!types.ts',
     '!src/provider/Village/Dev/index.ts',
   ],
-  coverageThreshold: {
-    'src/**/*.{js,jsx,ts,tsx}': {
-      // Set these all to 30 - we just want to ensure that a file actually gets covered, we're not too worried
-      // about actual coverage %ages _yet_
-      statements: 30,
-      branches: 30,
-      functions: 30,
-      lines: 30,
-    },
-  },
+  // coverageThreshold: {
+  //   'src/**/*.{js,jsx,ts,tsx}': {
+  //     // Set these all to 30 - we just want to ensure that a file actually gets covered, we're not too worried
+  //     // about actual coverage %ages _yet_
+  //     statements: 30,
+  //     branches: 30,
+  //     functions: 30,
+  //     lines: 30,
+  //   },
+  // },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$':
       'identity-obj-proxy',
