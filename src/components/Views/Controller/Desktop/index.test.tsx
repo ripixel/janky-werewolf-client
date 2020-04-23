155a765466835395898b69a71a99797811ceeddb
James King <ripixel@gmail.com>
2020-04-23 19:33:31 +0100

    Update Desktop Layout to match Mobile

--- src/components/Views/Controller/Desktop/index.test.tsx
+++ src/components/Views/Controller/Desktop/index.test.tsx
@@ -4,14 +4,10 @@ import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 
 import { DesktopViewController } from '.';
-import { PHASE_NAME } from '../../../../types/phase';
 
 // Mock the views with testids
 jest.mock('../../Desktop/NoPhase', () => (): JSX.Element => (
   <div data-testid='views-desktop-nophase' />
-));
-jest.mock('../../Desktop/Lobby', () => (): JSX.Element => (
-  <div data-testid='views-desktop-lobby' />
 ));
 
 describe('<DesktopViewController>', () => {
@@ -20,14 +16,6 @@ describe('<DesktopViewController>', () => {
       const result = render(<DesktopViewController />);
 
       expect(result.getByTestId('views-desktop-nophase')).toBeInTheDocument();
-    });
-
-    it('with lobby phase', () => {
-      const result = render(
-        <DesktopViewController phaseName={PHASE_NAME.LOBBY} />
-      );
-
-      expect(result.getByTestId('views-desktop-lobby')).toBeInTheDocument();
     });
   });
 });
--- src/components/Views/Desktop/Lobby/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export const DesktopLobbyView = (): JSX.Element => <p>Desktop Lobby</p>;
-
-export default DesktopLobbyView;
