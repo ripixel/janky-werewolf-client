0efc5584d907690d0b79e8f3a1e0878811121ac9
James King <ripixel@gmail.com>
2020-12-13 14:35:27 +0000

    TextOnly and VoteSinglePlayer views tests

--- src/components/Views/TextOnly/NonBodyguard/index.tsx
+++ src/components/Views/TextOnly/NonBodyguard/index.tsx
@@ -5,7 +5,7 @@ import TextOnly from '..';
 export const NonBodyguardTextOnly: React.FC = () => (
   <TextOnly
     title='You are not a Bodyguard'
-    description='The Bodyguards are busy saving people... pray its you'
+    description="The Bodyguards are busy saving people... pray it's you"
   />
 );
 
